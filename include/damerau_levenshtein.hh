9fc54909fc05e3a1f8594e7d2c86056644389592
Paul Villedieu <paul.villedieu@epita.fr>
2019-07-31 17:35:36 +0200

    include files: commented

--- include/damerau_levenshtein.hh
+++ include/damerau_levenshtein.hh
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+// Distance dl (damerau levenshtein), compute the distance for a given Trie
+// It takes a max distance and a word and call the recursive function distance_rec
 vector<tuple<string, unsigned short, unsigned>> distance_dl(Trie*& t, const string& word, unsigned short max_dist);
+
+// The recursive function used to browse all the nodes of the Trie and construct the distance matrix
+// It takes the current Trie node, the letter, the base word, the maximum distance, the previous row
+// of the matrix and the current word since the root of the Trie
 vector<tuple<string, unsigned short, unsigned>> distance_rec(Trie* t, char c, const string& word,
 		unsigned short max_dist, vector<unsigned short> previous_row, string current_word);
--- include/tries.hh
+++ include/tries.hh
@@ -15,16 +15,26 @@ using namespace std;
 class Trie {
 
 public:
+    /* Is the last char of a word ? */
     bool isWord;
+    /* Children of this Trie in tuple format <Trie, key> */
     std::vector<tuple<Trie*, char>> character;
+    /* Frequency of the word n if isWord 0 otherwise */
     unsigned freq;
 
+    // Constructor for the serialization part, read from the dictionary file
     Trie(const string& dictionary_file);
+    // Constructor for the deserialization
     Trie(bool isWord, unsigned freq);
 
+    // Iterative function to insert a key in the Trie
     void insert(const string& word, const unsigned& freq);
+    // Iterative function to search a key in Trie
+    // It returns true if the word is found in the Trie, otherwise false
     bool search(const string&);
+    // Method to display the Trie in a file, can be read with WebGraphViz
     void print_trie();
+    // Simple function to write line per line the Trie in a file
     void write_trie(ofstream& file, char c);
     /* Dump the Trie into a file */
     void serialize(FILE *fp, char key);
