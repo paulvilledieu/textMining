f3171d27bcba3b39d77c8f8ac752028de6891584
Paul Villedieu <paul.villedieu@epita.fr>
2019-07-31 16:19:05 +0200

    tests suite: up + Questions in README answered partially

--- README.txt
+++ README.txt
@@ -5,6 +5,24 @@ In order to run the code:
 	$ mkdir build && cd build && cmake ..
 	$ make
 
-QUESTIONS:
+To lauch the tests suite (at the root of the repo):
+	$ test/test.sh
 
-TO ANSWER
+QUESTIONS:
+	1 - 
+
+	2 - Nous avons créé une tests suite comprenant des tests sur le mot testé, la distance et aussi sur le mode d'entrée des tests (echo, cat , ...)
+	
+	3 - 
+	
+	4 - 
+	
+	5 - En ajoutant des outils d'analyse contextuel, un réglage automatique de la distance pourrait être mise en place: un exemple simple serait de dire
+		que des mots avec une distance importantes par rapport au mot testé mais avec une similarité importante pourrait être retourné par le programme
+		avec des mots ayant une distance beaucoup moins importante mais avec une similarité bien inférieure.
+
+	6 - Afin d'améliorer les performances de notre programme, nous pourrions dans un premier temps utiliser un Patricia Trie au lieu de notre Trie actuel. 
+		Ensuite, nous pourrions nettement améliorer nos performances de (dé)serialisation en utilisant des transformation de Trie vers des structures de type
+		Judy Array par exemple. Cela améliorerait d'une part la rapidité de notre programme, mais aussi la mémoire utilisée par celui-ci. 
+	
+	7 -
--- test/test.sh
+++ test/test.sh
@@ -4,12 +4,12 @@ test_total=0
 test_done=0
 
 printf '\n'
-ref/TextMiningCompiler test/test_words.txt ref/dict.bin > /dev/null
+ref/TextMiningCompiler words.txt ref/dict.bin > /dev/null
 
 echo "========== TEST $test_total =========="
 cm1='echo "approx 0 myfox"'
 echo "=> $cm1"
-if diff <(echo "approx 0 myfox" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 0 myfox" | ./TextMiningApp dict.bin 2> /dev/null)
+if diff <(echo "approx 0 myfox" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 0 myfox" | ./TextMiningApp dict.bin 2> /dev/null) > /dev/null
 then
     test_done=$((test_done + 1))
     echo "[OK]"
@@ -22,7 +22,7 @@ printf '\n'
 echo "========== TEST $test_total =========="
 cm1='echo "approx 1 myfox"'
 echo "=> $cm1"
-if diff <(echo "approx 1 myfox" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 1 myfox" | ./TextMiningApp dict.bin 2> /dev/null)
+if diff <(echo "approx 1 myfox" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 1 myfox" | ./TextMiningApp dict.bin 2> /dev/null) > /dev/null
 then
     test_done=$((test_done + 1))
     echo "[OK]"
@@ -35,7 +35,7 @@ printf '\n'
 echo "========== TEST $test_total =========="
 cm1='echo "approx 2 myfox"'
 echo "=> $cm1"
-if diff <(echo "approx 2 myfox" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 2 myfox" | ./TextMiningApp dict.bin 2> /dev/null)
+if diff <(echo "approx 2 myfox" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 2 myfox" | ./TextMiningApp dict.bin 2> /dev/null) > /dev/null
 then
     test_done=$((test_done + 1))
     echo "[OK]"
@@ -44,6 +44,102 @@ else
 fi
 test_total=$((test_total + 1))
 printf '\n'
+
+echo "========== TEST $test_total =========="
+cm1='echo "approx 0 test"'
+echo "=> $cm1"
+if diff <(echo "approx 0 test" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 0 test" | ./TextMiningApp dict.bin 2> /dev/null) > /dev/null
+then
+    test_done=$((test_done + 1))
+    echo "[OK]"
+else
+    echo "[KO]"
+fi
+test_total=$((test_total + 1))
+printf '\n'
+
+echo "========== TEST $test_total =========="
+cm1='echo "approx 1 test"'
+echo "=> $cm1"
+if diff <(echo "approx 1 test" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 1 test" | ./TextMiningApp dict.bin 2> /dev/null) > /dev/null
+then
+    test_done=$((test_done + 1))
+    echo "[OK]"
+else
+    echo "[KO]"
+fi
+test_total=$((test_total + 1))
+printf '\n'
+
+echo "========== TEST $test_total =========="
+cm1='echo "approx 2 test"'
+echo "=> $cm1"
+if diff <(echo "approx 2 test" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 2 test" | ./TextMiningApp dict.bin 2> /dev/null) > /dev/null
+then
+    test_done=$((test_done + 1))
+    echo "[OK]"
+else
+    echo "[KO]"
+fi
+test_total=$((test_total + 1))
+printf '\n'
+
+echo "========== TEST $test_total =========="
+cm1='echo "approx 3 test"'
+echo "=> $cm1"
+if diff <(echo "approx 3 test" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 3 test" | ./TextMiningApp dict.bin 2> /dev/null) > /dev/null
+then
+    test_done=$((test_done + 1))
+    echo "[OK]"
+else
+    echo "[KO]"
+fi
+test_total=$((test_total + 1))
+printf '\n'
+
+echo "========== TEST $test_total =========="
+cm1='echo "approx 4 test"'
+echo "=> $cm1"
+if diff <(echo "approx 4 test" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 4 test" | ./TextMiningApp dict.bin 2> /dev/null) > /dev/null
+then
+    test_done=$((test_done + 1))
+    echo "[OK]"
+else
+    echo "[KO]"
+fi
+test_total=$((test_total + 1))
+printf '\n'
+
+echo "========== TEST $test_total =========="
+cm1='echo "approx 0 test\napprox 1 test\napprox 2 test\napprox 3 test\napprox 4 test"'
+echo "=> $cm1"
+if diff <(echo "approx 0 test\napprox 1 test\napprox 2 test\napprox 3 test\napprox 4 test" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 0 test\napprox 1 test\napprox 2 test\napprox 3 test\napprox 4 test" | ./TextMiningApp dict.bin 2> /dev/null) > /dev/null
+then
+    test_done=$((test_done + 1))
+    echo "[OK]"
+else
+    echo "[KO]"
+fi
+test_total=$((test_total + 1))
+printf '\n'
+
+echo "========== TEST $test_total =========="
+cm1='cat test/test.txt'
+echo "=> $cm1"
+if diff <(cat test/test.txt | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(cat test/test.txt | ./TextMiningApp dict.bin 2> /dev/null) > /dev/null
+then
+    test_done=$((test_done + 1))
+    echo "[OK]"
+else
+    echo "[KO]"
+fi
+test_total=$((test_total + 1))
+printf '\n'
+
+
+
+
+
 
 echo "============================="
 echo "========== RESULTS =========="
