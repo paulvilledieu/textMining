c8da129cfbab6b44a91c3e4f35a513defabbba41
Paul Villedieu <paul.villedieu@epita.fr>
2019-07-31 13:47:51 +0200

    test/ dir added

---
+++ test/test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+test_total=0
+test_done=0
+
+printf '\n'
+ref/TextMiningCompiler src/test_words.txt ref/dict.bin > /dev/null
+
+echo "========== TEST $test_total =========="
+cm1='echo "approx 0 test"'
+echo "=> $cm1"
+if diff <(echo "approx 0 myfox" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 0 myfox" | TextMiningApp dict.bin 2> /dev/null) > /dev/null
+then
+    test_done=$((test_done + 1))
+    echo "[OK]"
+else
+    echo "[KO]"
+fi
+test_total=$((test_total + 1))
+printf '\n'
+
+echo "========== TEST $test_total =========="
+cm1='echo "approx 1 myfox"'
+echo "=> $cm1"
+if diff <(echo "approx 1 myfox" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 1 myfox" | TextMiningApp dict.bin 2> /dev/null) > /dev/null
+then
+    test_done=$((test_done + 1))
+    echo "[OK]"
+else
+    echo "[KO]"
+fi
+test_total=$((test_total + 1))
+printf '\n'
+
+echo "========== TEST $test_total =========="
+cm1='echo "approx 2 myfox"'
+echo "=> $cm1"
+if diff <(echo "approx 2 myfox" | ref/TextMiningApp ref/dict.bin 2> /dev/null) <(echo "approx 2 myfox" | TextMiningApp dict.bin 2> /dev/null) > /dev/null
+then
+    test_done=$((test_done + 1))
+    echo "[OK]"
+else
+    echo "[KO]"
+fi
+test_total=$((test_total + 1))
+printf '\n'
+
+echo "============================="
+echo "========== RESULTS =========="
+echo "============================="
+
+echo $test_done / $test_total
